e4c6da79f37d3b054ca3671b87067dadc72558e5
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-03-11 22:40:03 +0530

    feat: assets, UINavigationControllers, segue, new NavigationControllers, and isEnabled for buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,24 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var RecordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordButton.isEnabled = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func RecordAudio(_ sender: Any) {
-        print("Record button pressed!")
+        //print("Record button pressed!")
         RecordingLabel.text = "Recording Audio..."
+        recordButton.isEnabled = false
+        stopRecordButton.isEnabled = true
     }
 
     @IBAction func StopRecording(_ sender: Any) {
-        print("StopRecording pressed")
+        //print("StopRecording pressed")
+        recordButton.isEnabled = true
+        stopRecordButton.isEnabled = false
+        RecordingLabel.text = "Tap to record"
     }
 }
 
